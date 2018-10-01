ca1af69fca6c38813911bb9c6b255147b1ca02be
Asma <asma.virgo85@gmail.com>
2018-10-01 10:26:22 +0100

    creates database tables

---
+++ db/spending_tracker.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS currencies;
+
+CREATE TABLE currencies (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  short_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE merchants (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tags (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users (
+  id SERIAL4 PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  budget FLOAT8
+);
+
+CREATE TABLE transactions (
+  id SERIAL4 PRIMARY KEY,
+  user_id INT4 REFERENCES users(id),
+  tag_id INT4 REFERENCES tags(id),
+  merchant_id INT4 REFERENCES merchants(id),
+  currency_id INT4 REFERENCES currencies(id),
+  amount FLOAT8,
+  trx_time timestamp
+);
